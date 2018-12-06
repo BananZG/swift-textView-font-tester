ec024e698c08e785d73810f53b63068b36c1414d
Gan Jun Jie <junjie.gan@sicmsb.com>
2018-12-06 10:48:18 +0800

    Initial Commit

---
+++ playSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  playSwift
+//
+//  Created by Gan Jun Jie on 06/12/2018.
+//  Copyright © 2018 Gan Jun Jie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
